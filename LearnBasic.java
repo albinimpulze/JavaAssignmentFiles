fb9b6ca644e167fbdd2744a571686948b308beaf
Albin Xavier Varghese <115107643+albinimpulze@users.noreply.github.com>
2023-12-21 13:18:16 +0530

    Add files via upload

---
+++ LearnBasic.java
@@ -0,0 +1,41 @@
+package LearnLoops;
+
+public class LearnBasic
+{
+//        Loops :
+//        Infinite
+//        do while
+//        while
+//        for
+    public static void main(String[] args)
+    {
+        int x = 1; //11
+        do
+        {
+            System.out.println(x);
+            x++;
+        }
+        while(x<11);
+
+        int y = 1;
+        while(y<11)
+        {
+            System.out.println(y);
+            y++;
+        }
+
+        for(int i=1;i<11;i++)
+        {
+            System.out.println(i);
+
+        }
+
+
+
+
+
+
+    }
+
+}
+
